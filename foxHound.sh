72244883f18101935c572666c0e738fefde867e3
Emilio Garcia <emilio97@gmail.com>
2024-05-08 22:33:28 -0400

    Create foxHound.sh

---
+++ foxHound.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+catchOrRelease () {
+        if [[ $1 == "/Applications/Firefox.app" ]]; then
+                echo "$1 - valid install location"
+        else
+                echo "$1 - invalid install location"
+                rm -rf $1
+        fi
+}
+export -f catchOrRelease
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Warning! Not running as root. All files may not be found"
+fi
+
+mdfind "kMDItemCFBundleIdentifier = org.mozilla.firefox" -0 | xargs -0 bash -c 'for arg; do catchOrRelease "$arg"; done' _
+
+find /Users/*/.Trash -name Firefox.app -exec rm -rf "{}" +
